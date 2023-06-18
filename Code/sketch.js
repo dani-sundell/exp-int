53873583df1a0009b57b08a813888b64bda3f2c0
dani-sundell <93359879+dani-sundell@users.noreply.github.com>
2023-06-18 15:57:52 -0400

    Final Code

--- Code/sketch.js
+++ Code/sketch.js
@@ -8,6 +8,31 @@ let trackIds = ["spotify:track:5QThTYJveDHE40f4zODdyo", "spotify:track:5inqbGqSc
 
 //trail
 let trail = [];
+var drawTrail;
+
+//control clck
+let controlKey = false;
+
+  //eye coords
+  var eyeW;
+  var eyeH;
+
+  var eyeX1;
+  var eyeX2;
+  var eyeY1;
+  var eyeY2;
+
+//text overlay opacity
+
+var textFillOpacity;
+var imgAlpha;
+
+function preload() {
+  iconCrtl = loadImage('assets/ctrl.svg');
+  iconLeftClick = loadImage('assets/left click.svg');
+  iconRightClick = loadImage('assets/right click.svg');
+  iconEye = loadImage('assets/eye.svg');
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -15,6 +40,18 @@ function setup() {
     angleMode(DEGREES);
     colorMode(HSB, 360, 100, 100, 100);
     noStroke();
+
+    //eye coords
+    eyeW = 64;
+    eyeH = 32;
+    eyeX1 = width/2 - eyeW/2;
+    eyeX2 = width/2 + eyeW/2;
+    eyeY1 = height - 72 - eyeH/2;
+    eyeY2 = height - 72 + eyeH/2;
+
+    textFillOpacity = 1;
+    imgAlpha = 255;
+    drawTrail = true;
 }
 
 function draw() {
@@ -28,45 +65,107 @@ function draw() {
   } else if (currentGameIndex === 3) {
     deathsDoor();
   }
- 
-  //Text
+
+  drawingContext.fillStyle = `rgba(255, 255, 255, ${textFillOpacity})`;
+
+  //Header
   textFont('bookmania');
   textStyle(ITALIC);
   textAlign(CENTER);
-  textSize(72);
-  text(gameTitle[currentGameIndex], width/2, height/2);
+  textSize(96);
+  text('Relax & sketch', width/2, height/2);
+
+  //Subhead
+  textFont('roc-grotesk');
+  textSize(18);
+  textStyle(NORMAL);
+  text('to some of my favorite tracks in various video games', width/2, height/2 + 36);
+  
+  //Instrutions
+  tint(255, imgAlpha);
+  textAlign(RIGHT);
+  textSize(18);
+  text('use right click', width - 440, 72);
+  image(iconRightClick, width - 434, 52, 32, 32);
+  text('or', width - 376, 72);
+  image(iconCrtl, width - 364, 52, 32, 32);
+  text('+', width - 314, 72);
+  image(iconLeftClick, width - 310, 52, 32, 32);
+  text('to change scene & song', width - 72, 72);
+
+  textAlign(LEFT);
+  textSize(18);
+  text('use left click', 72, 72);
+  image(iconLeftClick, 184, 52, 32, 32);
+  text('to draw', 222, 72);
+
+  //Game Name
+  textAlign(RIGHT);
+
   textFont('roc-grotesk');
   textStyle(NORMAL);
-  textAlign(RIGHT);
   textSize(16);
-  text('use right click to change scene & song', width - 72, 72);
-  textAlign(LEFT);
-  textSize(16);
-  text('use left click to draw', 72, 72);
-  textAlign(RIGHT);
-  textSize(16);
-  textWrap(WORD);
-  text('Relax and sketch to some of my favorite tracks in various video games', width  * .76, height * .9, width * .2, height * .25);
+  text('current track from', width - 72, height - 108);
 
+  textFont('bookmania');
+  textStyle(ITALIC);
+  textSize(24);
+  text(gameTitle[currentGameIndex], width - 72, height - 72);
+
+  drawingContext.fillStyle = `rgba(255, 255, 255, 1)`;
+  textFont('roc-grotesk');
+  textStyle(NORMAL);
+
+  //eye mapping
+  if (mouseX > eyeX1 && mouseX < eyeX2 && mouseY > eyeY1 && mouseY < eyeY2) {
+    textAlign(CENTER);
+    textSize(12);
+    text('show / hide overlay', width/2, eyeY1 - eyeH);
+  }
+  tint(255, 255);
+  eyeToggle = image(iconEye, eyeX1, eyeY1, eyeW, eyeH);
+
+  if (mouseX > eyeX1 && mouseX < eyeX2 && mouseY > eyeY1 && mouseY < eyeY2) {
+    drawTrail = false;
+  } else {
+    drawTrail = true;
+  } 
+}
+
+function keyPressed() {
+  if (keyCode === CONTROL) {
+    controlKey = true;
+    console.log(controlKey, "Control pressed");
+  }
+}
+
+function keyReleased() {
+  if (keyCode === CONTROL) {
+    controlKey = false;
+    console.log(controlKey, "NOT pressed");
+  }
+}
+
+function mouseClicked() {
+  if (textFillOpacity == 1 && mouseX > eyeX1 && mouseX < eyeX2 && mouseY > eyeY1 && mouseY < eyeY2) {
+    textFillOpacity = 0;
+    imgAlpha = 0;
+    console.log('eye was clicked!');
+  } else if (textFillOpacity == 0 && mouseX > eyeX1 && mouseX < eyeX2 && mouseY > eyeY1 && mouseY < eyeY2) {
+    textFillOpacity = 1;
+    imgAlpha = 255;
+  } 
 }
 
 function mouseReleased() {
-  if (mouseButton === RIGHT) {
+  if (mouseButton === RIGHT || controlKey === true) {
+  console.log("this is a right click");
   currentGameIndex = (currentGameIndex + 1) % gameTitle.length;
   currentSongIndex = (currentSongIndex + 1) % songTitle.length;
   currentUriIndex =  (currentUriIndex + 1) % trackIds.length;
   trail.length = 0;
   }
 }
-
-function keyPressed() {
-  if (key === ' ') {
-    currentGameIndex = (currentGameIndex + 1) % gameTitle.length;
-    currentSongIndex = (currentSongIndex + 1) % songTitle.length;
-  }
-}
-
-
 
 function radialGradient(sX, sY, sR, eX, eY, eR, colorS, colorE){
   let gradient = drawingContext.createRadialGradient(
@@ -98,7 +197,7 @@ function animalCrossing() {
     color(310, 90, 100, 0), //End color
   );
   
- if (mouseIsPressed === true) {
+ if (mouseIsPressed === true && drawTrail != false) {
   trail.push(createVector(mouseX, mouseY));
   let followerSize = map(mouseY, 0, height, 10, 10);
   ellipse(mouseX, mouseY, followerSize, followerSize);
@@ -109,16 +208,7 @@ function animalCrossing() {
     ellipse(pos.x, pos.y, followerSize, followerSize);
   }
 };
-
-  fill('white');
-  radialGradient(
-    width/2, height/2-120, 0,//Start pX, pY, start circle radius
-    width/2, height/2-120, 380,//End pX, pY, End circle radius
-    color(0, 0, 100, 100), //Start color
-    color(0, 0, 100, 100), //End color
-  );
 }
-
 
 function residentEvil() {
 
@@ -139,7 +229,7 @@ function residentEvil() {
     color(330, 90, 60, 0), //End color
   );
   
-  if (mouseIsPressed === true) {
+  if (mouseIsPressed === true && drawTrail != false) {
     trail.push(createVector(mouseX, mouseY));
     let followerSize = map(mouseY, 0, height, 10, 10);
     ellipse(mouseX, mouseY, followerSize, followerSize);
@@ -150,16 +240,7 @@ function residentEvil() {
       ellipse(pos.x, pos.y, followerSize, followerSize);
     }
   };
-
-  fill('white');
-  radialGradient(
-    width/2, height/2-120, 0,//Start pX, pY, start circle radius
-    width/2, height/2-120, 380,//End pX, pY, End circle radius
-    color(0, 0, 100, 100), //Start color
-    color(0, 0, 100, 100), //End color
-  );
 }
-
 
 function stardewValley() {
 
@@ -180,7 +261,7 @@ function stardewValley() {
     color(150, 90, 80, 0), //End color
   );
   
-  if (mouseIsPressed === true) {
+  if (mouseIsPressed === true && drawTrail != false) {
     trail.push(createVector(mouseX, mouseY));
     let followerSize = map(mouseY, 0, height, 10, 10);
     ellipse(mouseX, mouseY, followerSize, followerSize);
@@ -191,14 +272,6 @@ function stardewValley() {
       ellipse(pos.x, pos.y, followerSize, followerSize);
     }
   };
-
-  fill('white');
-  radialGradient(
-    width/2, height/2-120, 0,//Start pX, pY, start circle radius
-    width/2, height/2-120, 380,//End pX, pY, End circle radius
-    color(0, 0, 100, 100), //Start color
-    color(0, 0, 100, 100), //End color
-  );
 }
 
 function deathsDoor() {
@@ -206,8 +279,8 @@ function deathsDoor() {
   radialGradient(
     width + 120, height/2 - 120, 0,//Start pX, pY, start circle radius
     width, height/2-120, width,//End pX, pY, End circle radius
-    color(140, 40, 20, 100), //Start color
-    color(240, 10, 5, 100), //End color
+    color(140, 70, 20, 100), //Start color
+    color(240, 10, 20, 100), //End color
   );
   rect(0, 0, width, height);
   
@@ -220,7 +293,7 @@ function deathsDoor() {
     color(90, 30, 60, 0), //End color
   );
   
-  if (mouseIsPressed === true) {
+  if (mouseIsPressed === true && drawTrail != false) {
     trail.push(createVector(mouseX, mouseY));
     let followerSize = map(mouseY, 0, height, 10, 10);
     ellipse(mouseX, mouseY, followerSize, followerSize);
@@ -231,14 +304,6 @@ function deathsDoor() {
       ellipse(pos.x, pos.y, followerSize, followerSize);
     }
   };
-
-  fill('white');
-  radialGradient(
-    width/2, height/2-120, 0,//Start pX, pY, start circle radius
-    width/2, height/2-120, 380,//End pX, pY, End circle radius
-    color(0, 0, 100, 100), //Start color
-    color(0, 0, 100, 100), //End color
-  );
 }
 
 function windowResized() {
@@ -254,10 +319,6 @@ window.onSpotifyIframeApiReady = (IFrameAPI) => {
     theme: 'dark'
   };
   const callback = (EmbedController) => {
-    EmbedController.addListener('ready', e => {
-      console.log('ready');
-      EmbedController.play(); 
-    });
     EmbedController.addListener('playback_update', e => {
       if (e.data.position === 0 && e.data.isPaused === true) {
         EmbedController.seek(1);
@@ -265,13 +326,21 @@ window.onSpotifyIframeApiReady = (IFrameAPI) => {
       if (e.data.position === 1000 && e.data.isPaused === true) {
             EmbedController.play();   
       };
-    });    
+    });
+    document.addEventListener("mousedown",function(event) { // listen for control + click to change track uri
+      if (event.ctrlKey || event.button == 2) {
+        EmbedController.loadUri(trackIds[currentUriIndex]);
+        EmbedController.play(); 
+      } else { 
+        EmbedController.play();
+      }
+    }, );
     document.body.addEventListener('contextmenu', function(ev) { // listen for right click to change track uri
       ev.preventDefault();
       EmbedController.loadUri(trackIds[currentUriIndex]);
       EmbedController.play(); 
       return false;
-  }, false);
+    }, false);
   };
   IFrameAPI.createController(element, options, callback);
 };
